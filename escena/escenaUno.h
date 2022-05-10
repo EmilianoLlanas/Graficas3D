5d36a1c44532ab8d202ac9787863dae37b291721
Luis Angel <luisangel.diaz188@gmail.com>
2022-05-09 21:28:54 -0500

    Arboles y animacion

--- escena/escenaUno.h
+++ escena/escenaUno.h
@@ -3,11 +3,13 @@
 
 #include "../utilidades/Op3D.h"
 #include "../objetos/arbol.h"
+#include "../objetos/piramide.h"
 
 class EscenaUno{
 private:
     Op3D *op;
     Arbol arbol1;
+    Piramide piramide;
     float pointsA[3], pointsB[3];
     void loadIdentity();
 
