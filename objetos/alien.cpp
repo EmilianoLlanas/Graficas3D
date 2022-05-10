e58d9b405b0c002a86deab064f6d79b9acbba457
Samantha Estrada <36958888+samjulliet@users.noreply.github.com>
2022-05-09 21:15:35 -0500

    Alien completo

--- objetos/alien.cpp
+++ objetos/alien.cpp
@@ -15,32 +15,32 @@ void Alien::loadPoints(){
     points[0][0] = 0;
     points[1][0] = 0;
     points[2][0] = 0;
-    points[3][0] = 2;
+    points[3][0] = 1;
 
     points[0][1] = 4;
     points[1][1] = 0;
     points[2][1] = 0;
-    points[3][1] = 2;
+    points[3][1] = 1;
 
     points[0][2] = 4;
     points[1][2] = 0;
     points[2][2] = 4;
-    points[3][2] = 2;
+    points[3][2] = 1;
 
     points[0][3] = 0;
     points[1][3] = 0;
     points[2][3] = 4;
-    points[3][3] = 2;
+    points[3][3] = 1;
 
     points[0][4] = 2;
     points[1][4] = 3;
     points[2][4] = 2;
-    points[3][4] = 2;
+    points[3][4] = 1;
 
     points[0][5] = 2;
     points[1][5] = -3;
     points[2][5] = 2;
-    points[3][5] = 2;
+    points[3][5] = 1;
 }
 
 void Alien::draw(){
@@ -50,11 +50,14 @@ void Alien::draw(){
     GLfloat colors [3] = {0.42f, 0.66f, 1.77f};
 
     GLint width = 3;
+    GLdouble fillDif = 0.1;
+
+    int i;
+    GLdouble j;
 
     glColor3fv(colors);
     glPointSize(width);
 
-    int i;
     glBegin(GL_LINE_LOOP);
       for(i=0;i<4;i++)
         glVertex3d((*p)[0][i],(*p)[1][i],(*p)[2][i]);
@@ -73,4 +76,63 @@ void Alien::draw(){
         glVertex3d((*p)[0][i],(*p)[1][i],(*p)[2][i]);
       }
     glEnd();
+
+    //Fill
+    glBegin(GL_LINES);
+      for(j=(*p)[0][0]; j<(*p)[0][1];j = j + fillDif){
+        glVertex3d(j,(*p)[1][0],(*p)[2][0]);
+        glVertex3d((*p)[0][4],(*p)[1][4],(*p)[2][4]);
+      }
+    glEnd();
+
+    glBegin(GL_LINES);
+      for(j=(*p)[0][0]; j<(*p)[0][1];j = j + fillDif){
+        glVertex3d(j,(*p)[1][0],(*p)[2][0]);
+        glVertex3d((*p)[0][5],(*p)[1][5],(*p)[2][5]);
+      }
+    glEnd();
+
+    glBegin(GL_LINES);
+      for(j=(*p)[0][2]; j>(*p)[0][3];j = j - fillDif){
+        glVertex3d(j,(*p)[1][2],(*p)[2][2]);
+        glVertex3d((*p)[0][4],(*p)[1][4],(*p)[2][4]);
+      }
+    glEnd();
+
+    glBegin(GL_LINES);
+      for(j=(*p)[0][2]; j>(*p)[0][3];j = j - fillDif){
+        glVertex3d(j,(*p)[1][2],(*p)[2][2]);
+        glVertex3d((*p)[0][5],(*p)[1][5],(*p)[2][5]);
+      }
+    glEnd();
+
+    glBegin(GL_LINES);
+      for(j=(*p)[2][1]; j<(*p)[2][2];j = j + fillDif){
+        glVertex3d((*p)[0][1],(*p)[1][1],j);
+        glVertex3d((*p)[0][4],(*p)[1][4],(*p)[2][4]);
+      }
+    glEnd();
+
+    glBegin(GL_LINES);
+      for(j=(*p)[2][1]; j<(*p)[2][2];j = j + fillDif){
+        glVertex3d((*p)[0][1],(*p)[1][1],j);
+        glVertex3d((*p)[0][5],(*p)[1][5],(*p)[2][5]);
+      }
+    glEnd();
+
+    glBegin(GL_LINES);
+      for(j=(*p)[2][3]; j>(*p)[2][0];j = j - fillDif){
+        glVertex3d((*p)[0][3],(*p)[1][3],j);
+        glVertex3d((*p)[0][4],(*p)[1][4],(*p)[2][4]);
+      }
+    glEnd();
+
+    glBegin(GL_LINES);
+      for(j=(*p)[2][3]; j>(*p)[2][0];j = j - fillDif){
+        glVertex3d((*p)[0][3],(*p)[1][3],j);
+        glVertex3d((*p)[0][5],(*p)[1][5],(*p)[2][5]);
+      }
+    glEnd();
+
+
 }
